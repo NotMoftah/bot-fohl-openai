83185a77961762ca24fc992b5b0b6db57814b186
AbdElAziz Moftah <abdelazizimoftah@gmail.com>
2026-05-28 00:51:17 +0200

    remove OpenAI dependencies and simplify message handling

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,30 +2,18 @@ import os
 import json
 import asyncio
 
-from transformers import OpenAIChatBot
-from utils import LambdaLogger, TimeTools, WebTools
+from utils import LambdaLogger
 from telegram_bot import TelegramMessage, FohlFehBot, LambdaRequestParser
 
 # Set up logging
 logger = LambdaLogger()
 
-
 # Get the bot token from environment variables
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-GPT_TOKEN = os.getenv("GPT_TOKEN")
 
 
-# Initialize the GPT model
-language_model = OpenAIChatBot(GPT_TOKEN)
-language_model.register_external_tools(TimeTools())
-language_model.register_external_tools(WebTools())
-
-
-async def on_private_message(message: TelegramMessage) -> None:
-    try:
-        return await language_model.send_message(message.text)
-    except Exception as e:
-        logger.error(f"Error in translate handler: {e}", exc_info=True)
+async def on_private_message(message: TelegramMessage) -> str:
+    return message.text
 
 
 # Initialize the bot
